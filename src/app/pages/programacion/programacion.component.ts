934b229a59eaae639bdf66f9d9e2962b215064bd
richmtx <martinezhernandezricardo06@gmail.com>
2026-08-14 12:49:24 -0600

    Commit

---
+++ src/app/pages/programacion/programacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programacion',
+  imports: [],
+  templateUrl: './programacion.component.html',
+  styleUrl: './programacion.component.css'
+})
+export class ProgramacionComponent {
+
+}
---
+++ src/app/pages/videoteca/videoteca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-videoteca',
+  imports: [],
+  templateUrl: './videoteca.component.html',
+  styleUrl: './videoteca.component.css'
+})
+export class VideotecaComponent {
+
+}
